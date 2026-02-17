65146bba2bc9b2c8bf005749dfd67c359ae220b8
apiippajaaa <afifmisbahuddin7@gmail.com>
2026-02-18 01:26:56 +0700

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "/src/css/style.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Afif-Portfolio-React/">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
